7f4455269c3c6a0e54666fdc49e4fb8457ae5ac6
ahmedsh100 <ahmedmohamedsh100@gmail.com>
2026-02-28 21:27:13 +0200

    Update...

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+                <div class="col-md-12 bg-dark">
+                    <div class="p-5 wow fadeInUp" data-wow-delay="0.2s">
+                        <h5 class="section-title ff-secondary text-start text-primary fw-normal">Register</h5>
+                        <h1 class="text-white mb-4">Register for a new user</h1>
+                        <form method="POST" action="{{ route('register') }}" class="col-md-12">
+                            <div class="row g-3">
+                                <div class="">
+                                    <div class="form-floating">
+                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                        <label for="name">Name</label>
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                                    </div>
+                                </div>
+                                <div class="">
+                                    <div class="form-floating">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                        <label for="email">Email address</label>
+                                    </div>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                                </div>
+
+                                <div class="">
+                                <div class="form-floating">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <label for="password">Password</label>
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                                </div>
+                                </div>
+
+                                <div class="">
+                                <div class="form-floating">
+
+                                <input id="password-config" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <label for="password-config">Confirm Password</label>
+                                </div>
+
+
+                                <div class="col-md-12">
+                                    <button class="btn btn-primary w-100 py-3" name="submit" type="submit">Register</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +131,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
